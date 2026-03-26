7e32a8e45414a085791eeb2dfac8db4eb32c0e7a
dinhkhoi124 <dinhkhoi1work@gmail.com>
2026-03-26 11:23:39 +0700

    Remove explicitly ignored files

--- Backend/test_supabase.py
+++
@@ -1,6 +0,0 @@
-from supabase_client import supabase
-
-# Lấy danh sách bảng có trong database
-tables = supabase.table("profiles").select("*").limit(1).execute()
-print("✅ Kết nối thành công! Dữ liệu mẫu:")
-print(tables)
